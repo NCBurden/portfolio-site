c6cdedc7c68db30cbf66818a46c6c40e6b2f938a
Chase Burden <chase.burden00@gmail.com>
2024-06-20 22:08:20 -0500

    Finally added something to the site!

---
+++ src/data.js
@@ -0,0 +1,47 @@
+//TODO: Fill this out!
+export const projects = [
+    {
+        title: "HERDR",
+        subtitle: "MERN stack",
+        description: "An app to match college room mates based on their answers to certain questions.",
+        image: require("./resources/herdr logo.png"),
+        link: "",
+    },
+    {
+        title: "Project 2",
+        subtitle: "MERN stack",
+        description: "lorem ipsum ...",
+        image: "",
+        link: "",
+    }
+]
+
+export const skills = [
+    "Java",
+    "Python",
+    "Javascript",
+    "React",
+    "GitHub",
+    "GCP"
+]
+
+export const testimonials = [
+    {
+        quote: "He Does Excellent Work!",
+        image: "",
+        name: "Joe",
+        company: "Joe Mama"
+    },
+    {
+        quote: "WOW what a GREAT worker",
+        image: "",
+        name: "Donald",
+        company: "Executive Branch"
+    },
+    {
+        quote: "He Does Excellent Work!",
+        image: "",
+        name: "",
+        company: ""
+    }
+]
