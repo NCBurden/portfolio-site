4e73b3499769a9a9bc9b494b877b50c997892c56
Chase Burden <chase.burden00@gmail.com>
2024-06-26 17:29:26 -0500

    Editing Middleware

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -2,8 +2,10 @@ import staticFormsPlugin from "@cloudflare/pages-plugin-static-forms";
 
 export const onRequest: PagesFunction = staticFormsPlugin({
   respondWith: ({ formData, name }) => {
-    const email = formData.get('email')
-    console.log(email)
+    const personName: FormDataEntryValue | null = formData.get('name');
+    const email: FormDataEntryValue | null = formData.get('email');
+    const message: FormDataEntryValue | null = formData.get('message');
+    console.log(`Name: ${personName}`)
     return new Response(`Hello, ${email}! Thank you for submitting the ${name} form.`)
   }
 });
