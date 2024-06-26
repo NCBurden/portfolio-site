9ce1522cd76911ea8b0c3a7bff04989d4c53eccc
Chase Burden <chase.burden00@gmail.com>
2024-06-26 17:52:26 -0500

    Added semicolons

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -5,7 +5,7 @@ export const onRequest: PagesFunction = staticFormsPlugin({
     const personName: FormDataEntryValue | null = formData.get('name');
     const email: FormDataEntryValue | null = formData.get('email');
     const message: FormDataEntryValue | null = formData.get('message');
-    console.log(`Name: ${personName}`)
-    return new Response(`Hello, ${email}! Thank you for submitting the ${name} form.`)
+    console.log(`Name: ${personName}`);
+    return new Response(`Hello, ${email}! Thank you for submitting the ${name} form.`);
   }
 });
