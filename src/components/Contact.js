e19187b8630e58c8128b34053dabe780ab5722b6
Chase Burden <chase.burden00@gmail.com>
2024-06-26 22:03:19 -0500

    Testing more!

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,8 +18,7 @@ export default function Contact() {
             // headers: {"Content-Type": "application/x-www-form-urlencoded"},
             // body: encode({"form-name": "contact", name, email, message}),
         })
-        .then(res => {res.json();
-        alert(res)})
+        .then(res => alert(res))
         .then(data => alert(`Message Sent! ${data}`))
         .catch((error) => alert(error));
     }
